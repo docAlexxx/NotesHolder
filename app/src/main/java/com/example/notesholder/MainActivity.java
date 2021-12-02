b0a0a524a8aecd04e3337b3b458805c650ff0eae
docAlexxx <docalex@mail.ru>
2021-12-02 23:54:59 +0400

    standart toolbar changed to own toolbar

--- app/src/main/java/com/example/notesholder/MainActivity.java
+++ app/src/main/java/com/example/notesholder/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.notesholder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.TextView;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            for (int i = 0; i < Notes.notes.length; i++) {
-            Notes.notes[i] = new Notes(i, "Note" + (i+1), "description" + (i + 1) + " and many other different words about something", "2" + (i + 1) + ".11.2021");
-            };
+        for (int i = 0; i < Notes.notes.length; i++) {
+            Notes.notes[i] = new Notes(i, "Note" + (i + 1), "description" + (i + 1) + " and many other different words about something", "2" + (i + 1) + ".11.2021");
+        };
 
         if (savedInstanceState == null) {
             NotesFragment notesFragment = new NotesFragment();
@@ -24,12 +26,40 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.fragment_container, notesFragment)
                     .commit();
         }
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.about_point:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .replace(R.id.fragment_container, new InfoFragment())
+                        .commit();
+                return true;
+            case R.id.settings_point:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .replace(R.id.fragment_container, new SettingsFragment())
+                        .commit();
+                return true;
+            case R.id.exit_point:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/example/notesholder/SettingsFragment.java
+++ app/src/main/java/com/example/notesholder/SettingsFragment.java
@@ -14,4 +14,5 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
 }
