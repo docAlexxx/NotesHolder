cbe55d0bfc7a3d1bd01c441adf1915d3f96d6997
docAlexxx <docalex@mail.ru>
2021-12-03 20:34:19 +0400

    added actions for navigation drawer

--- app/src/main/java/com/example/notesholder/MainActivity.java
+++ app/src/main/java/com/example/notesholder/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.notesholder;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,9 +31,40 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.fragment_container, notesFragment)
                     .commit();
         }
+        setupToolbar();
+    }
 
+    private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setupNavigationDrawer(toolbar);
+    }
+
+    private void setupNavigationDrawer(Toolbar toolbar) {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.about_point:
+                    openAboutFragment();
+                    drawer.closeDrawers();
+                    return true;
+                case R.id.settings_point:
+                    openSettingsFragment();
+                    drawer.closeDrawers();
+                    return true;
+                case R.id.exit_point:
+                    finish();
+                    return true;
+            }
+            return false;
+        });
     }
 
     @Override
@@ -42,24 +78,32 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         switch (id) {
             case R.id.about_point:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .addToBackStack("")
-                        .replace(R.id.fragment_container, new InfoFragment())
-                        .commit();
+                openAboutFragment();
                 return true;
             case R.id.settings_point:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .addToBackStack("")
-                        .replace(R.id.fragment_container, new SettingsFragment())
-                        .commit();
+                openSettingsFragment();
                 return true;
             case R.id.exit_point:
                 finish();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openAboutFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack("")
+                .replace(R.id.fragment_container, new InfoFragment())
+                .commit();
+    }
+
+    private void openSettingsFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack("")
+                .replace(R.id.fragment_container, new SettingsFragment())
+                .commit();
     }
 
 }
