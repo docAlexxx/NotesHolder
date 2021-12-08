a625a0bb988b7463d6d32e35c74ca190ffb7a031
docAlexxx <docalex@mail.ru>
2021-12-09 00:39:38 +0400

    lesson 10 init

--- app/src/main/java/com/example/notesholder/Utils.java
+++ app/src/main/java/com/example/notesholder/Utils.java
@@ -3,9 +3,12 @@ package com.example.notesholder;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 
+import androidx.appcompat.app.ActionBar;
+
 public class Utils {
     public static boolean isLandscape(Resources res) {
         return res.getConfiguration().orientation
                 == Configuration.ORIENTATION_LANDSCAPE;
     }
+
 }
