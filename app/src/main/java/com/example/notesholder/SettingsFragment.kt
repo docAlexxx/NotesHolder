7f90d456889c7f90a58c6bae1b18c73332b745c5
docAlexxx <docalex@mail.ru>
2021-12-23 19:46:56 +0400

    Lesson1: convert SettingFragment to Kotlin

--- app/src/main/java/com/example/notesholder/SettingsFragment.kt
+++ app/src/main/java/com/example/notesholder/SettingsFragment.kt
@@ -1,25 +1,22 @@
-package com.example.notesholder;
+package com.example.notesholder
 
-import android.os.Bundle;
+import com.example.notesholder.Utils.setSubtitleName
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.notesholder.R
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-public class SettingsFragment extends Fragment {
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
-        ActionBar actionBar = ((AppCompatActivity)requireActivity()).getSupportActionBar();
-
-        Utils.setSubtitleName(actionBar,"Settings");
-
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+class SettingsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+        setSubtitleName(actionBar, "Settings")
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
