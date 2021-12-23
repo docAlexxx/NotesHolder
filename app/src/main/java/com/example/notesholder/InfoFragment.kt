4af565c363eec021a207780517a44cc9cf31ff2a
docAlexxx <docalex@mail.ru>
2021-12-23 19:48:31 +0400

    Lesson1: convert InfoFragment to Kotlin

--- app/src/main/java/com/example/notesholder/InfoFragment.kt
+++ app/src/main/java/com/example/notesholder/InfoFragment.kt
@@ -1,24 +1,22 @@
-package com.example.notesholder;
+package com.example.notesholder
 
-import android.os.Bundle;
+import com.example.notesholder.Utils.setSubtitleName
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.notesholder.R
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-public class InfoFragment extends Fragment {
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        setHasOptionsMenu(true);
-        ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
-        Utils.setSubtitleName(actionBar, "Info");
-
-        return inflater.inflate(R.layout.fragment_info, container, false);
+class InfoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+        setSubtitleName(actionBar, "Info")
+        return inflater.inflate(R.layout.fragment_info, container, false)
     }
 }
